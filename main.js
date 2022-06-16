827e18bf9aac4bb250b33814569f264b495ccad0
Patrickyyh <yeyuhao1234@tamu.edu>
2022-06-16 03:34:45 -0500

    continue tomorrow

--- main.js
+++ main.js
@@ -6,12 +6,19 @@ let token = null;
 
 // create the uid for each user who would like to join the room. 
 let uid = String(Math.floor(Math.random() * 10000));
-
-
-
 let channel;
 let client ; 
 
+// get the query params from the URL 
+let queryString = window.location.search
+let urlParams = new URLSearchParams(queryString)
+let roomId = urlParams.get('room');
+
+if(!roomId){
+    // redirect the user back to the looby if no roomId query. 
+    window.location = 'lobby.html'
+
+}
 
  // const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.come:19302'}]};
     // peerConnection = new RTCPeerConnection(configuration);
@@ -57,25 +64,26 @@ let init = async()=>{
 
      //index.html?room = 234234
      // create the channel and join the channel
-    channel = client.createChannel('main');
+     // get roomId from the query parameter 
+    channel = client.createChannel(roomId);
     await channel.join(); 
     
     // adds the listener function to the channel 
     // for the event named (eventName)
-    channel.on('MemberJoined',    handleUserJoined);
+    channel.on('MemberJoined',   handleUserJoined);
 
     // handle the message with another handler by the client 
     client.on('MessageFromPeer' ,handleMessageFromPeer);
 
+    // event listener listens for the the client leaves the channel
+    channel.on('MemberLeft', handleUserLeft);
 
-    
-    
     // sending the request permission to the camera feed 
     // The call to get user media will trigger a permission request 
     // getUserMedia will return a MediaStream 
     localStream =  await navigator.mediaDevices.getUserMedia(LocaldeviceRequest);
-    
     // Local playback
+    
     document.getElementById('user-1').srcObject = localStream; 
 
     
@@ -85,34 +93,60 @@ let init = async()=>{
 let  handleMessageFromPeer =  async (message , MemberUID)=>{
     //console.log("message: ", message.text);
     message = JSON.parse(message.text)
-    
-    
     console.log("message: ", message);
+    
+
+    // Set up the UDP offer and answer
+    if(message.type === 'offer'){
+        createAnswer(MemberUID,message.offer); 
+
+    }
+
+    if(message.type === 'answer'){
+        addAnswer(message.answer); 
+    }
+
+    // set up the ICE candidate
+    
+    if(message.type === 'candidate'){
+            
+        if(peerConnection){
+          await peerConnection.addIceCandidate(message.candidate);
+         }
+    }  
+ }
 
 
-}
 
 let handleUserJoined = async (MemberUID) =>{
     console.log('A new User joind the channel: fe',MemberUID);
     createOffer(MemberUID); 
-    
 }
 
-let createOffer =    async (MemberUID) => {
+
+let handleUserLeft = async () => {
+    // this action only take effect when the user really leaves the channel. 
+    document.getElementById('user-2').style.display = 'none';
+}
+
+
+let createPeerConnection = async (MemberUID)=> {
     
-   
+    
     peerConnection = new RTCPeerConnection(server);
     remoteStream   = new MediaStream();
     document.getElementById('user-2').srcObject = remoteStream;
+    // show the second screen when the user-2 joins the room. 
+    document.getElementById('user-2').style.display = 'block';
+    
 
 
     // retrive all the tracks of audio and video by calling getTracks
     // and add the sets of tracks and transmit it to other peer. 
-   
     if(!localStream){
         localStream =  await navigator.mediaDevices.getUserMedia(LocaldeviceRequest);
+        document.getElementById('user-1').srcObject = localStream; 
     }
-
 
     localStream.getTracks().forEach(track => {
         peerConnection.addTrack(track,localStream);
@@ -121,24 +155,28 @@ let createOffer =    async (MemberUID) => {
      // event listner and listen for track comes from another user 
      peerConnection.ontrack =  (event)=>{
          event.streams[0].getTracks().forEach((track) => {
-             remoteStream.addTrack()
+             remoteStream.addTrack(track);
          })
      }
 
-     
+   
      // Trickle ICE 
      // Gathr ice candidates , listen for local ice candidates 
      peerConnection.addEventListener('icecandidate' ,event => {
          if(event.candidate){
             client.sendMessageToPeer({text: JSON.stringify({'type': 'candidate', 'candidate': event.candidate})},MemberUID);
-            document.getElementById('user-1').srcObject = localStream; 
         }
      }); 
 
-     // Listen for remote ICE candidate and add them to the local RTCpeerConnection 
+}
 
-     
-     
+
+
+let createOffer =    async (MemberUID) => {
+    
+    
+     // Listen for remote ICE candidate and add them to the local RTCpeerConnection 
+     await createPeerConnection(MemberUID);
     // createk offer and send the offer 
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
@@ -147,12 +185,36 @@ let createOffer =    async (MemberUID) => {
     
     client.sendMessageToPeer({text: JSON.stringify({'type': 'offer', 'offer': offer})},MemberUID); 
 
-    k
+
 
 }
 
-let createAnswer  =  async (MemberUID) => {
-    
+
+// create SDP AND ICE answer for the second user. 
+let createAnswer  =  async (MemberUID,offer) => {
+
+    await createPeerConnection(MemberUID);
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(answer);
+    client.sendMessageToPeer({text: JSON.stringify({'type': 'answer' , 'answer': answer})}, MemberUID); 
+
+
 }
+
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription){
+        peerConnection.setRemoteDescription(answer); 
+    }
+
+}
+
+
+let leaveChannel = async () => {
+    await channel.leave();
+    await client.logout();
+}
+
+window.addEventListener('beforeunload', leaveChannel); 
 
 init();
